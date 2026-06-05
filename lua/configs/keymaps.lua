997b02bf4c8c013aba9cdb4dc8e9842fd6d187ea
Brandon <kongbrandon0@gmail.com>
2026-06-05 15:54:30 -0500

    feat(keymaps): added QOL keymaps for pasting, navigating

--- lua/configs/keymaps.lua
+++ lua/configs/keymaps.lua
@@ -1,39 +1,51 @@
 local builtin = require("telescope.builtin")
 local harpoon = require("harpoon")
 
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
 -- Vim cmd remaps
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-d>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set("n", "J", "mzJ`z")
+
+vim.keymap.set("i", "<C-c>", "<Esc>")
 
 -- Telescope builtin remaps
 vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
 vim.keymap.set("n", "<C-p>", builtin.git_files, {})
 vim.keymap.set("n", "<leader>ps", function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+    builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 
 -- Goto definition remaps
 vim.api.nvim_create_autocmd("LspAttach", {
-	callback = function(e)
-		local opts = { buffer = e.buf }
-		vim.keymap.set("n", "gd", function()
-			vim.lsp.buf.definition()
-		end, opts)
-		vim.keymap.set("n", "K", function()
-			vim.lsp.buf.hover()
-		end, opts)
-		vim.keymap.set("n", "<leader>vrr", function()
-			vim.lsp.buf.references()
-		end, opts)
-		vim.keymap.set("n", "<leader>vrn", function()
-			vim.lsp.buf.rename()
-		end, opts)
-	end,
+    callback = function(e)
+        local opts = { buffer = e.buf }
+        vim.keymap.set("n", "gd", function()
+            vim.lsp.buf.definition()
+        end, opts)
+        vim.keymap.set("n", "K", function()
+            vim.lsp.buf.hover()
+        end, opts)
+        vim.keymap.set("n", "<leader>vrr", function()
+            vim.lsp.buf.references()
+        end, opts)
+        vim.keymap.set("n", "<leader>vrn", function()
+            vim.lsp.buf.rename()
+        end, opts)
+    end,
 })
 
 -- Harpoon remaps
 vim.keymap.set("n", "<leader>a", function()
 	harpoon:list():add()
 end)
+
 vim.keymap.set("n", "<C-e>", function()
 	harpoon.ui:toggle_quick_menu(harpoon:list())
 end)
