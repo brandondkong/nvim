88c67b71e8ab9ac966008635962883148610cbac
Brandon <kongbrandon0@gmail.com>
2026-06-05 14:18:00 -0500

    feat: added pull rebase map

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -15,6 +15,9 @@ return {
 
 		vim.keymap.set("n", "<leader>T", ":Git push -u origin ", opts)
 		vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
+		vim.keymap.set("n", "<leader>P", function()
+			vim.cmd.Git({ "pull", "--rebase" })
+		end, opts)
 		vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")
 	end,
 }
