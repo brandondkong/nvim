b343fbfd2a3285700527322b4a2747891c7b15f7
Brandon <kongbrandon0@gmail.com>
2026-06-05 15:56:12 -0500

    chore: fixed fugitive push keymaps

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -3,21 +3,17 @@ return {
 	config = function()
 		vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
-		local buffer = vim.api.nvim_get_current_buf()
-		local opts = { buffer = buffer, remap = false }
 		vim.keymap.set("n", "<leader>p", function()
 			vim.cmd.Git("push")
-		end, opts)
+		end)
 
 		vim.keymap.set("n", "<leader>P", function()
 			vim.cmd.Git({ "pull", "--rebase" })
-		end, opts)
+		end)
 
-		vim.keymap.set("n", "<leader>T", ":Git push -u origin ", opts)
+		vim.keymap.set("n", "<leader>T", ":Git push -u origin ")
+
 		vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
-		vim.keymap.set("n", "<leader>P", function()
-			vim.cmd.Git({ "pull", "--rebase" })
-		end, opts)
 		vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")
 	end,
 }
