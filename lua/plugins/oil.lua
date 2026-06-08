25b22f04ef4ad49c7ec6e4d0d0a836dcdd13e2e1
Brandon <kongbrandon0@gmail.com>
2026-06-08 16:35:35 -0500

    fix: replaced conflicting keymaps with oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,6 +1,12 @@
 return {
     "stevearc/oil.nvim",
-    opts = {},
+    opts = {
+        use_default_keymaps = true,
+        keymaps = {
+            ["<C-p>"] = {},
+            ["<C-r>"] = "actions.preview",
+        }
+    },
     dependencies = { "nvim-mini/mini.icons" },
     lazy = false,
 }
