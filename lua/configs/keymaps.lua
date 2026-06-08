40ce4818e653feee28bb4c6906d61bf02a3e102f
Brandon <kongbrandon0@gmail.com>
2026-06-08 16:35:00 -0500

    chore: updated keymaps

--- lua/configs/keymaps.lua
+++ lua/configs/keymaps.lua
@@ -4,9 +4,9 @@ local harpoon = require("harpoon")
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- Vim cmd remaps
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", "<cmd>Oil<CR>")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-d>", "<C-u>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
